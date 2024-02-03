e552a63fefe8d61b8d8a79e0b4d2b6c1f7c91a02
BelalAboSeada <belalaboseada@gmail.com>
2024-02-03 22:43:08 +0200

    Resolved conflicts

--- src/Components/themeItem.jsx
+++ src/Components/themeItem.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
+
 
 const ThemeItem = ({ color, img, ChangeColor }) => {
   return <img src={img} className="theme_img" onClick={() => ChangeColor(color)} />;
 };
 
 export default ThemeItem;
-=======
-import React from "react";
-
-const ThemeItem = ({ color, img, ChangeColor }) => {
-  return <img src={img} className="theme_img" onClick={() => ChangeColor(color)} />;
-};
-
-export default ThemeItem;
->>>>>>> 61b7dec643d5f99903dc1bd44e8f63c4cfbbabc0
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { portfolio } from "../../data";
 import Card from "../../Components/Card/Card";
 import "./Style.scss";
@@ -19,26 +18,3 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
-=======
-import React from "react";
-import { portfolio } from "../../data";
-import Card from "../../Components/Card/Card";
-import "./Style.scss";
-
-const Portfolio = () => {
-  return (
-    <section className="portfolio section">
-      <h2 className="section__title">
-        My <span>Portfolio</span>
-      </h2>
-      <div className="Portfolio__container d-grid">
-        {portfolio.map((item) => (
-          <Card key={item.id} {...item} />
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default Portfolio;
->>>>>>> 61b7dec643d5f99903dc1bd44e8f63c4cfbbabc0
