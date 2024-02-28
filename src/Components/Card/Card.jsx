29bf14be1fd327abed501ba80f56075f68c05c55
BelalAboSeada <belalaboseada@gmail.com>
2024-02-28 22:59:54 +0200

    Update Portfolio Page

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,59 +1,33 @@
-import { useState } from "react";
-import { FaTimes } from "react-icons/fa";
-import { IoIosArrowForward } from "react-icons/io";
-import { Link } from "react-router-dom";
+import  { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-const Card = ({ id,img, title, details }) => {
-  const [modal, setModal] = useState(false);
-
-  const toggleModal = () => {
-    setModal(!modal);
-  };
+const Card = ({ id, img, title, details }) => {
+  const [selectedId, setSelectedId] = useState(null);
 
   return (
-    <div className="Portfolio_item">
-      <img
+    <motion.div className="Portfolio_item" layout>
+      <motion.img
         src={img}
         loading="lazy"
         type="image/webp"
         className="Portfolio_img"
         alt={title}
+        layoutId={`image-${id}`}
       />
       <div className="Portfolio_hover position-absolute d-grid">
         <h3 className="Portfolio_title">{title}</h3>
-        <div className="Btns">
-          <button className="Quick_Btn" onClick={toggleModal}>
-            Quick View <IoIosArrowForward />
-          </button>
-          <Link to={`/ProjectDetails/${id}`}  className="details_Btn">
-            Details <IoIosArrowForward />
-          </Link>
-        </div>
-        {modal && (
-          <div className="Portfolio_modal position-fixed d-grid">
-            <div className="Portfolio_modal_content position-relative d-grid">
-              <FaTimes
-                className="Close_modal position-absolute"
-                onClick={toggleModal}
-              />
-              <h3 className="modal_title">{title}</h3>
-              <ul className="modal_list d-grid">
-                {details.map(({ icon, title, desc }, index) => (
-                  <li className="modal_item d-flex align-items-center" key={index}>
-                    <span className="item_icon d-flex">{icon}</span>
-                    <div>
-                      <span className="item_title">{title}</span>
-                      <span className="item_details">{desc}</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-              <img src={img} alt={title} className="img_modal" />
-            </div>
-          </div>
-        )}
       </div>
-    </div>
+      <AnimatePresence>
+        {selectedId && (
+          <motion.div layoutId={selectedId}>
+            {/* Assuming item is not defined, you should use props */}
+            <motion.h5>{details}</motion.h5>
+            <motion.h2>{title}</motion.h2>
+            <motion.button onClick={() => setSelectedId(null)} />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,21 +1,20 @@
 import { portfolio } from "../../data";
 import Card from "../../Components/Card/Card";
+import { motion } from "framer-motion";
 import "./Style.scss";
 
 const Portfolio = () => {
-  return (   
-      <section className="portfolio section">
-        <h2 className="section__title">
-          My <span>Portfolio</span>
-        </h2>
-        <div className="Portfolio__container d-grid">
-          {portfolio.map((item) => (
-            <Card key={item.id} {...item} />
-          ))}
-        </div>
-      </section>
-  
-    
+  return (
+    <section className="portfolio section">
+      <h2 className="section__title">
+        My <span>Portfolio</span>
+      </h2>
+      <motion.div className="Portfolio__container d-grid" layout>
+        {portfolio.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </motion.div>
+    </section>
   );
 };
 
