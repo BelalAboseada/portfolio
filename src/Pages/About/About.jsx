020f491c4c492044796e665081732c1b57649bb7
BelalAboSeada <belalaboseada@gmail.com>
2024-06-21 20:34:23 +0300

    fix building problem

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,7 @@ import "./style.scss";
 import Skills from "./Skills/Skills";
 import Info from "./info/Info";
 import InternetConnection from "../../servies/internetConnection";
-import Loader from "../../Components/Loader/ContentLoader";
+import Loader from "../../Components/Loader/contentLoader";
 import { useState } from "react";
 
 function About() {
